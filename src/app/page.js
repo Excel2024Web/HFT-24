cbfbf87d0fe386ca58469cf867ada78e015b8314
Diya Therese Shibu <diyathereseshibu@gmail.com>
2024-10-12 15:18:43 +0530

    highlights

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import BlobAnimation from "./components/BlobAnimation";
 import Footer from "./components/Footer";
 import About from "./components/About";
+import Highlights from './components/highlights/highlight';
 import CountdownTimer from "./components/Countdown/Countdown";
 
 export default function Home() {
@@ -12,9 +13,9 @@ export default function Home() {
       {/* <CountdownTimer
         targetDate={new Date("December 21, 2024 09:30:00").getTime()}
       /> */}
-
+      
       <About />
-
+      <Highlights />
       <Footer />
     </>
   );
