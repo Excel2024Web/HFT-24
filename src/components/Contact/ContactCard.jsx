529a3d3448a5415fb9f57d46a7fc7976163c87ad
Nived R S <nivedrsalini@gmail.com>
2024-11-14 12:04:33 +0530

    gradient-exp

--- src/components/Contact/ContactCard.jsx
+++ src/components/Contact/ContactCard.jsx
@@ -1,16 +1,15 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react';
-import Image from 'next/image'; 
-import { FaLinkedinIn } from 'react-icons/fa';
-import { LuMails } from 'react-icons/lu';
-import { FaPhone } from 'react-icons/fa6';
-import { motion } from 'framer-motion'; 
-import './ContactCard.css';
+import React, { useState } from "react";
+import Image from "next/image";
+import { FaLinkedinIn } from "react-icons/fa";
+import { LuMails } from "react-icons/lu";
+import { FaPhone } from "react-icons/fa6";
+import { motion } from "framer-motion";
+import "./ContactCard.css";
 
-const ContactCard = ({details}) => {
+const ContactCard = ({ details }) => {
   const [isHovered, setIsHovered] = useState(false);
-
 
   const cardVariants = {
     hidden: { opacity: 0, y: 50 },
@@ -18,40 +17,39 @@ const ContactCard = ({details}) => {
     exit: { opacity: 0, y: -50, transition: { duration: 0.4 } },
   };
 
- 
   const containerVariants = {
     visible: {
       opacity: 1,
       transition: {
-        staggerChildren: 0.3, 
+        staggerChildren: 0.3,
       },
     },
     hidden: { opacity: 0 },
   };
 
   const childVariants = {
-    visible: { 
-      opacity: 1, 
+    visible: {
+      opacity: 1,
       y: -15,
-      transition: { duration: 0.5 }  
+      transition: { duration: 0.5 },
     },
-    hidden: { 
-      opacity: 0, 
+    hidden: {
+      opacity: 0,
       y: 20,
-      transition: { duration: 0.5 }
+      transition: { duration: 0.5 },
     },
   };
 
   return (
     <>
-      <motion.div
+      {/* <motion.div
         className="card-container"
-        variants={cardVariants}            
-                
-        exit="exit"           
-        viewport={{ once: false }}    
-        onMouseEnter={() => setIsHovered(true)}  
-        onMouseLeave={() => setIsHovered(false)} 
+        variants={cardVariants}
+
+        exit="exit"
+        viewport={{ once: false }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
       >
         <div className="logowrapper">
           <Image
@@ -68,7 +66,7 @@ const ContactCard = ({details}) => {
         <motion.div
           className="socials"
           variants={containerVariants}
-          
+
           animate={isHovered ? 'visible' : 'visible'}
         >
           <motion.a href={details.linkedin} variants={childVariants} target='blank'>
@@ -81,7 +79,48 @@ const ContactCard = ({details}) => {
             <FaPhone />
           </motion.a>
         </motion.div>
-      </motion.div>
+      </motion.div> */}
+      <div
+        className="card-container"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <div className="background-image">
+          <Image
+            src={details.img}
+            alt="background"
+            fill={true}
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+        <div className="text-field z-10 flex flex-col justify-end items-start w-full h-full p-5">
+          <h2>{details.name}</h2>
+          <p>{details.title}</p>
+          {/* <motion.div
+            className="socials p-5"
+            variants={containerVariants}
+            animate={isHovered ? "visible" : "visible"}
+          >
+            <motion.a
+              href={details.linkedin}
+              variants={childVariants}
+              target="blank"
+            >
+              <FaLinkedinIn />
+            </motion.a>
+            <motion.a
+              href={`https://mail.google.com/mail/?view=cm&fs=1&to=${details.email}`}
+              variants={childVariants}
+              target="blank"
+            >
+              <LuMails />
+            </motion.a>
+            <motion.a href={`tel:${details.phone}`} variants={childVariants}>
+              <FaPhone />
+            </motion.a>
+          </motion.div> */}
+        </div>
+      </div>
     </>
   );
 };
