9170222044431eb6665e396b5e53b8884721ba5e
Niya Shiyas <niyashiyas03@gmail.com>
2024-11-16 19:53:26 +0530

    landing

--- src/components/BlobAnimation.jsx
+++ src/components/BlobAnimation.jsx
@@ -1,8 +1,12 @@
 import Spline from "@splinetool/react-spline";
 import React, {useEffect, useRef} from "react";
 import astro from "@/assets/Landing/astro.svg";
+import astro2 from "@/assets/Landing/astro2.svg";
+import astro3 from "@/assets/Landing/astro3.svg";
 import right from "@/assets/Landing/right.svg";
 import topleft from "@/assets/Landing/topleft.svg";
+import constel from "@/assets/Landing/constellation.svg";
+import arrow from "@/assets/Landing/arrow.svg";
 import Image from "next/image";
 
 const BlobAnimation = () => {
@@ -276,7 +280,12 @@ const BlobAnimation = () => {
           alt="topleft"
         />
         <Image
-          className="absolute right-0 bottom-0 z-0"
+          className="hidden lg:block absolute overflow-clip mt-[-4rem] mr-[-4rem] sm:mt-[-2rem] sm:mr-[-2rem] right-0 z-0"
+          src={constel}
+          alt="constellation"
+        />
+        <Image
+          className="hidden lg:block absolute bg-cover right-0 bottom-0 z-0"
           src={right}
           alt="bot-right"
         />
@@ -286,28 +295,39 @@ const BlobAnimation = () => {
           data-aos-duration="1100"
         >
           <Image
-            className="absolute scale-125 sm:scale-100 bottom-0 right-4 sm:right-0 w-[100%] sm:w-[84%] z-0"
-            src={astro}
+            className="hidden sm:block absolute bottom-0 sm:bottom-0 sm:right-[-8rem] z-0"
+            src={astro2}
             alt="astro"
           />
+          <Image
+            className="sm:hidden absolute bottom-0 sm:bottom-0 right-0 sm:right-[-4rem] z-0"
+            src={astro3}
+            alt="astro-mob"
+          />
 
-          <div className="flex z-10 gap-1 flex-col items-center sm:items-start">
-            {/* <div className="text-white font-base-neue-bold max-w-[100vw] md:text-[6rem] font-bold text-left leading-[90px] max-lg:text-[82px] max-lg:max-w-[480px] max-lg:leading-[70px] md:max-w-[700px] small-screen:text-[50px] small-screen:leading-[50px]"> */}
-            <div className="uppercase text-white font-base-neue-bold max-w-[100vw] text-5xl md:text-[6rem] font-bold leading-[90px] max-lg:text-[65px] max-lg:max-w-[480px] max-lg:leading-[70px] md:max-w-[700px] small-screen:text-[50px] small-screen:leading-[50px] text-center sm:text-left">
-              Hack For Tomorrow
+          <div className="flex z-10 flex-col gap-24 sm:gap-12 items-center sm:items-start">
+            <div className="flex flex-col gap-1 mt-[-8rem] sm:mt-0">
+              <div className="uppercase  text-center sm:text-left text-white font-base-neue-black max-w-[100vw] text-5xl md:text-[6rem] font-bold leading-[90px] max-lg:text-[61px] max-lg:max-w-[480px] max-lg:leading-[70px] md:max-w-[700px] small-screen:text-[50px] small-screen:leading-[50px]">
+                Hack For Tomorrow
+              </div>
+              <p
+                data-aos="fade-up"
+                data-aos-duration="1100"
+                className="text-[21px] font-[500] font-syne-medium max-lg:text-[20px] max-md:text-[18px] text-center sm:text-left"
+                // className="text-[21px] font-[500] font-syne-medium max-lg:text-[20px] max-md:text-[18px] text-center sm:text-left"
+              >
+                Innovate Today, Impact Tomorrow
+              </p>
             </div>
-            <p
-              data-aos="fade-up"
-              data-aos-duration="1100"
-              className="text-[21px] font-[500] font-syne-medium max-lg:text-[20px] max-md:text-[18px] "
-              // className="text-[21px] font-[500] font-syne-medium max-lg:text-[20px] max-md:text-[18px] text-center sm:text-left"
-            >
-              Innovate Today, Impact Tomorrow
-            </p>
             {/* <button className="btn-hover mt-10 rounded-full mx-auto sm:mx-0 font-syne-semibold bg-white font-bold uppercase text-[1rem] text-black px-3 py-3 max-w-[200px]"> */}
-            <button className="btn-hover mt-10 rounded-full font-syne-semibold bg-white font-bold uppercase text-[1rem] text-black px-3 py-3 max-w-[200px]">
-              Register Now
-            </button>
+            <div className="bg-white px-2 my-auto flex rounded-full font-syne-semibold font-bold">
+              <p className="text-center pl-2 flex items-center btn-hover uppercase text-[1rem] text-black">
+                Register Now
+              </p>
+              <span className=" rounded-full">
+                <Image src={arrow} className="scale-75"></Image>
+              </span>
+            </div>
           </div>
         </div>
       </div>
