ccd4d04ea8a4c172f4f2c9dfe9fd5beb81cb913b
Niya Shiyas <niyashiyas03@gmail.com>
2024-11-17 13:35:05 +0530

    cleaned, contacts, timeline etc

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -34,12 +34,12 @@ const FAQ = () => {
   return (
     <div className="faq">
       <div className="faq-container container mx-auto px-10 py-10 flex flex-col items-start">
-        <h1 className="font-bold text-start faq-heading">
+        <h1 className="font-base-neue-bold text-4xl sm:text-6xl text-start faq-heading">
           FREQUENTLY ASKED
           <br />
           QUESTIONS .
         </h1>
-        <div className="flex flex-col w-full pt-10">
+        <div className="font-syne-medium flex flex-col w-full pt-10">
           {QA.map((qa, index) => (
             <FAQCard
               key={index}
