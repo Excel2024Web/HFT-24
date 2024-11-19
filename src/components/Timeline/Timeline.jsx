0cdfa582e231171f615e3419bcafedc0d39d2aff
Nived R S <nivedrsalini@gmail.com>
2024-11-19 18:53:40 +0530

    Correct few margin to avoid Navbar getting overlapped on mobile

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -13,13 +13,13 @@ const TimelineSection = () => {
     >
       <div className="m-auto container font-generalSans min-h-screen flex justify-center flex-col items-center text-our_white">
         <h2
-          className="font-base-neue-bold text-6xl tracking-widest mirror-vertical text-center text-white mt-[30px] xl:scale-100 lg:scale-90 md:scale-75 scale-[.65] text-[#FFFFFF]/[0.8] "
+          className="font-base-neue-bold text-6xl tracking-widest mirror-vertical text-center text-white mt-[60px] xl:scale-100 lg:scale-90 md:scale-75 scale-[.65] text-[#FFFFFF]/[0.8] "
           data-text="TIMELINE"
         >
           TIMELINE
         </h2>
         <div
-          className="font-syne-medium mt-[90px] w-[900px] max-lg:w-[800px] max-md:w-[500px] max-sm:w-[350px] md:scale-110 max-sm:mt-[54px]"
+          className="font-syne-medium mt-[60px] w-[900px] max-lg:w-[800px] max-md:w-[500px] max-sm:w-[350px] md:scale-110 max-sm:mt-[54px]"
           data-aos="zoom-in-up"
         >
           <Timeline className="font-syne-medium max-md:hidden">
