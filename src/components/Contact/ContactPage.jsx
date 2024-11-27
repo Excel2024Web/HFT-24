bf413a6e91cce329da56be76f45b9fc81e246c0a
Niya Shiyas <niyashiyas03@gmail.com>
2024-11-27 14:41:57 +0530

    navbar font, nikhil add, gtag add

--- src/components/Contact/ContactPage.jsx
+++ src/components/Contact/ContactPage.jsx
@@ -2,7 +2,7 @@ import ContactCard from "./ContactCard";
 import chairperson from "../../assets/contacts/sreeparvathy_crop.jpg";
 import gensec from "../../assets/contacts/arif.jpg";
 import jointsec from "../../assets/contacts/akash.jpg";
-import eventhead from "../../assets/contacts/Harin.jpg";
+import eventhead from "../../assets/contacts/Nikhil.jpg";
 import eventhead2 from "../../assets/contacts/Rohit.jpg";
 import cstech from "@/assets/contacts/vaid.jpg";
 
@@ -45,11 +45,11 @@ export default function Contact() {
     },
 
     {
-      name: "Harin V",
+      name: "Nikhil M",
       title: "Event Head",
-      email: "harinvchirappanath@gmail.com",
-      phone: "+916282668713",
-      linkedin: "https://www.linkedin.com/in/harin-v-chirappanath-755669242",
+      email: "nikhil.mecheril@gmail.com",
+      phone: "+918129574473",
+      linkedin: "https://www.linkedin.com/in/nikhil-mecheril",
       img: eventhead,
     },
 
@@ -64,12 +64,16 @@ export default function Contact() {
   ];
   return (
     <>
-      <div id = "contacts" className="w-[100vw] flex justify-center items-center flex-col overflow-hidden h-full py-10 mt-[80px]">
+      <div
+        id="contacts"
+        className="w-[100vw] flex justify-center items-center flex-col overflow-hidden h-full py-10 mt-[80px]"
+      >
         <h1 className="mt-10 text-5xl tracking-wider text-center text-white max-md:text-4xl font-base-neue-bold">
           CONTACT US
         </h1>
         <p className="text-center font-syne-medium mt-5 max-md:mt-3 mb-10 max-md:text-sm bg-gradient-to-r bg-clip-text text-white text-[18px] mx-[30px]">
-          If you have questions, feedback, or just want to chat, feel free to reach out to us.
+          If you have questions, feedback, or just want to chat, feel free to
+          reach out to us.
         </p>
         <div className="flex flex-wrap justify-center item-center gap-[28px] max-w-[1200px] mt-[30px]">
           {contactDetails.map((cd, index) => (
