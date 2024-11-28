e636ce5cf91062bdfd35f0a4c4c00c1dbd14993d
Devadutt <devaduttmohancs@gmail.com>
2024-11-28 17:37:11 +0530

    info change and other changes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,12 +10,12 @@ import circle3 from "../../assets/backgrounds/about/circle3.png";
 
 const About = () => {
   return (
-    <div className="relative overflow-hidden">
+    <div className="relative">
       <div className="absolute bg-gradient-to-b from-[#0a0a0a] from-10% to-transparent z-50 bg-no-repeat right-0 left-0 top-0 min-h-32" />
       <div id="about" className="about-container pt-[80px]">
         <Image className="pointer-events-none top-left" src={top_left} />
         <Image
-          className="pointer-events-none bottom-right z-0"
+          className="pointer-events-none bottom-rightt z-0"
           src={bottom_right}
         />
         <Image className="pointer-events-none circle1" src={circle1} />
